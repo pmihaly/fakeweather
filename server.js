7774764f8bbc9b764d2bb624055ef5c6011ea0aa
pmihaly <mishreg@protonmail.com>
2020-08-01 15:40:39 +0200

    prettify

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 
 const app = express();
-app.use(express.urlencoded({extended:false}))
+app.use(express.urlencoded({ extended: false }))
 
 app.get('/historic-weather/:city/:date/:apiKey', (req, res) => {
     if (req.params.apiKey != "szuperbiztosjelszo") res.status("401").send("Unauthorized");
-    res.json({mintemp:"13", maxtemp:"25"});
+    res.json({ mintemp: "13", maxtemp: "25" });
 })
 
-app.listen(process.env.PORT || 3000, ()=>console.log('Server started...'));
+app.listen(process.env.PORT || 3000, () => console.log('Server started...'));
